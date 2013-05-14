20490674994432a76ad8a4436569f24760aa06fd
Vladimir Nazarenko <nazarenko2010@gmail.com>
2013-05-14 11:45:23 +0400

    UniqueList tests and comments added

--- Homework_5/UniqueList/UniqueList.cs
+++ Homework_5/UniqueList/UniqueList.cs
@@ -5,6 +5,16 @@ namespace Homework5
 
     public class UniqueList<T> : MyClasses.DataStructures.LinkedList<T>
     {
+        /// <summary>
+        ///  Inserts value after given List Element. Throws exception if the <paramref name="value"/>
+        /// was found in the list.
+        /// </summary>
+        /// <param name='value'>
+        /// Value to be inserted.
+        /// </param>
+        /// <param name='position'>
+        /// Position where to put value.
+        /// </param>
         public override void InsertAfter(T value, ListElement<T> position)
         {
             if (this.Find(value) == null)
@@ -17,6 +27,13 @@ namespace Homework5
             }
         }
 
+        /// <summary>
+        ///  Inserts value on the first position. Throws exception if the <paramref name="value"/>
+        /// was found in the list.
+        /// </summary>
+        /// <param name='value'>
+        ///  Value to be inserted. 
+        /// </param>
         public override void InsertFirst(T value)
         {
             if (this.Find(value) == null)
--- Homework_5/UniqueListTests/UniqueListTests.cs
+++ Homework_5/UniqueListTests/UniqueListTests.cs
@@ -15,5 +15,15 @@ namespace UniqueListTests
             list.Add(15);
             list.Add(15);
         }
+
+        [Test]
+        public void Insert_BunchOfvalues_Success()
+        {
+            var list = new UniqueList<int>();
+            list.Add(15);
+            list.Add(16);
+            Assert.IsTrue(list.Contains(15));
+            Assert.IsTrue(list.Contains(16));
+        }
     }
 }
