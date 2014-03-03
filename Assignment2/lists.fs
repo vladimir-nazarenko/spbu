c7d03a97ca480684d98900694ef74fa2a156a57c
Vladimir Nazarenko <nazarenko2010@gmail.com>
2014-03-03 11:17:59 +0400

    Fixed tail recursion

--- Assignment2/lists.fs
+++ Assignment2/lists.fs
@@ -1,13 +1,18 @@
 module Lists
 
-let rec reverse = function
-  | [] -> []
-  | head :: tail -> (reverse tail) @ [head]
+let reverse l =
+  let rec reverseTR lst constructed =
+    match lst with
+      | [] -> constructed
+      | x :: xs -> reverseTR xs (x :: constructed)
+  reverseTR l []
 
-let rec powersOfTwo = function
-    | n when n < 0 -> []
-    | n -> (pown 2 n) :: powersOfTwo (n - 1)
-
+let powersOfTwo n =
+  let rec helper m cur =
+    match m with
+      | m when m < 0 -> cur
+      | m -> helper m ((pown 2 m) :: cur)
+  helper n []
 let firstFivePowerOfTwo = reverse (powersOfTwo 5)
 
 let splitString (str: string) =
