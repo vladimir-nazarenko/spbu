b41f81190564c6a96488ae506c7761c34cecb79b
Vladimir Nazarenko <nazarenko2010@gmail.com>
2014-03-18 23:51:16 +0400

    Tried to implement sequential iterator

---
+++ Assignment5/IterableTree.fs
@@ -0,0 +1,73 @@
+module IterableTree
+
+open System.Collections.Generic
+
+// Binary tree representation
+type Tree<'a> =
+  | Tree of 'a * Tree<'a> option* Tree<'a> option
+  | Leaf of 'a
+
+// I believe, there is no need to make this function tail-recursive, while it can't cause stack overflow
+let rec addIntoTree t x =
+  match t with
+    | None -> Some(Leaf x)
+    | Some(Leaf y) -> if x > y then Some(Tree(y, None, Some(Leaf x))) else Some(Tree(y, Some(Leaf x), None))
+    | Some(Tree(z, l, r)) -> if x > z then Some(Tree(z, l, addIntoTree r x)) else Some(Tree(z, addIntoTree l x, r))
+
+let rec findInTree t x =
+  match t with
+    | None -> false
+    | Some(Leaf y) -> x = y
+    | Some(Tree(z, l, r)) -> if x > z then findInTree r x else findInTree l x
+
+let rec deleteFromTree t x =
+  let rec rightmost t =
+    match t with
+      | None -> None
+      | Some(Leaf x) -> Some(Leaf x)
+      | Some(Tree(y, l, r)) -> if r.IsSome then rightmost r else Some(Tree(y, l, r))
+  match t with
+    | None -> None
+    | Some(Leaf y) -> if x = y then None else Some (Leaf y)
+    | Some(Tree(z, l, r)) ->
+      if x > z then deleteFromTree r x
+      elif x < z then deleteFromTree l x
+      else
+        if l.IsSome && r.IsSome then
+          match rightmost l with
+            | None -> None
+            | Some(Leaf u) -> Some(Tree(u, l, r))
+            | Some(Tree(z', l', r')) -> Some(Tree(z', deleteFromTree l z', r))
+        elif l.IsSome then l
+        elif r.IsSome then r
+        else None
+
+exception IteratorException
+
+type ItTree<'a when 'a: comparison>() =
+  let mutable tree: Tree<'a> option = None
+  member t.find elem = findInTree tree elem
+  member t.insert elem = tree <- addIntoTree tree elem
+  member t.remove elem = tree <- deleteFromTree tree elem
+  type TreeEnum<'a>() =
+    let mutable stack = []
+    let buildStack tree st =
+      match tree with
+        | None -> []
+        | Some(Leaf x) -> (Leaf x) :: st
+        | Some(Tree x l r) -> (Tree x l r) :: (buildStack l)
+    stack <- buildStack tree stack
+    interface IEnumerator<'a> with
+      member e.Current =
+        match stack with
+          | [] -> raise IteratorException
+          | x :: xs ->
+            match x with
+              | Leaf u -> stack <- stack.Tail; u
+              | Tree(v l r) -> if 
+      member e.MoveNext() = true
+      member e.Dispose() = ()
+      member e.Reset() = ()
+
+
+    
