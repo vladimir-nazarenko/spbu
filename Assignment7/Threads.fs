ba2c7290a5181f3f8f502a90c8bec5dec575011f
Vladimir Nazarenko <nazarenko2010@gmail.com>
2014-04-06 14:49:28 +0400

    Tried to brake syncronization

--- Assignment7/Threads.fs
+++ Assignment7/Threads.fs
@@ -5,35 +5,34 @@ open System
 open System.Threading
 
 let arrayOfOnes = Array.create 1000000 1
+let sum = ref 0
 
 // sum pice of the array and return result
-let rec sumFromToTR(array: int [], s: int, e: int, result) =
+let rec sumFromToTR(array: int [], s: int, e: int) =
   match e - s with
-    | x when x < 0 -> result
-    | _ -> sumFromToTR(array, s + 1, e, result + array.[s])
+    | x when x < 0 -> ()
+    | _ -> sum := !sum + array.[s]; sumFromToTR(array, s + 1, e)
 
 // sum array of size 1m in 100 threads
 let sumArrayInThreads() =
   let workers: BackgroundWorker [] = Array.map (fun _ -> new BackgroundWorker()) [|0..99|]
   // events which would be triggered, when worker is done
   let finishEvents = Array.map (fun _ -> new System.Threading.AutoResetEvent(false)) [|0..99|]
-  let sum = ref 0
   let assignWorker n =
     let start = n * 10000
     let fin = (n + 1) * 10000 - 1
-    (workers.[n]).DoWork.Add(fun args ->
-                             sum := !sum + sumFromToTR(arrayOfOnes, start, fin, 0);
-                             finishEvents.[n].Set() |> ignore
-                             )
+    (workers.[n]).DoWork.Add(fun args -> sumFromToTR(arrayOfOnes, start, fin))
+    (workers.[n]).RunWorkerCompleted.Add(fun args -> finishEvents.[n].Set() |> ignore)
   Array.iter assignWorker [|0..99|]
   let startWorker(w: BackgroundWorker) = w.RunWorkerAsync()
   // wait for workers
   let activateEventLoop(e: System.Threading.AutoResetEvent) = e.WaitOne() |> ignore
   Array.iter startWorker workers
   Array.iter activateEventLoop finishEvents
-  !sum
 
-printf "result: %i\n" <| sumArrayInThreads()
+sumArrayInThreads()
+
+printf "result: %i\n" !sum
 printf "Finished\n"
 
 
