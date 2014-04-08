d57be95a85cf171487af47e336927f1053bdd3e4
Vladimir Nazarenko <nazarenko2010@gmail.com>
2014-04-08 17:03:35 +0400

    Added debug output

--- Assignment7/Threads.fs
+++ Assignment7/Threads.fs
@@ -4,7 +4,9 @@ open System.ComponentModel
 open System
 open System.Threading
 
-let arrayOfOnes = Array.create 1000000 1
+let arrayLength = 1000000
+let numberOfThreads = 10
+let arrayOfOnes = Array.create arrayLength 1
 let sum = ref 0
 
 // sum pice of the array and return result
@@ -15,15 +17,22 @@ let rec sumFromToTR(array: int [], s: int, e: int) =
 
 // sum array of size 1m in 100 threads
 let sumArrayInThreads() =
-  let workers: BackgroundWorker [] = Array.map (fun _ -> new BackgroundWorker()) [|0..99|]
+  let workers: BackgroundWorker [] = Array.map (fun _ -> new BackgroundWorker()) [|0..numberOfThreads - 1|]
   // events which would be triggered, when worker is done
-  let finishEvents = Array.map (fun _ -> new System.Threading.AutoResetEvent(false)) [|0..99|]
+  let finishEvents = Array.map (fun _ -> new System.Threading.AutoResetEvent(false)) [|0..numberOfThreads - 1|]
   let assignWorker n =
-    let start = n * 10000
-    let fin = (n + 1) * 10000 - 1
-    (workers.[n]).DoWork.Add(fun args -> sumFromToTR(arrayOfOnes, start, fin))
-    (workers.[n]).RunWorkerCompleted.Add(fun args -> finishEvents.[n].Set() |> ignore)
-  Array.iter assignWorker [|0..99|]
+    let start = n * (arrayLength / numberOfThreads)
+    let fin = (n + 1) * (arrayLength / numberOfThreads) - 1
+    (workers.[n]).DoWork.Add(
+      fun args ->
+        printf "Worker %i started\n" n;
+        sumFromToTR(arrayOfOnes, start, fin);
+        printf "Thread %i processed data\n" n)
+    (workers.[n]).RunWorkerCompleted.Add(
+      fun args ->
+        printf "Worker %i finished\n %O" n args.Error;
+        finishEvents.[n].Set() |> ignore)
+  Array.iter assignWorker [|0..numberOfThreads - 1|]
   let startWorker(w: BackgroundWorker) = w.RunWorkerAsync()
   // wait for workers
   let activateEventLoop(e: System.Threading.AutoResetEvent) = e.WaitOne() |> ignore
