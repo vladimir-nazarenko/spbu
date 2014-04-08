b0fb7601afa8dd7b473659ae32f6866e1f55c9dc
Vladimir Nazarenko <nazarenko2010@gmail.com>
2014-04-08 16:23:16 +0400

    Fixed previous work

--- Test1/Task3.fs
+++ Test1/Task3.fs
@@ -2,11 +2,18 @@ module PQ
 
 type PQueue<'a when 'a: equality>() =
   let mutable elems = []
+  let rec removeFirst lst elem built =
+    match lst with
+      | [] -> []
+      | x :: xs when x = elem -> (List.rev built) @ xs
+      | x :: xs -> removeFirst xs elem (x :: built)
   member p.enqueue(x: 'a, pr: int) = elems <- (x, pr) :: elems
   member p.dequeue =
     if elems.IsEmpty
-    then None else
+    then None
+    else
       let elem = (List.rev (List.sortBy(fun (a, b) -> b) elems)).Head
-      List.filter ((<>) elem) elems
+      elems <- removeFirst elems elem []
       Some(fst elem)
-  
+    
+    
