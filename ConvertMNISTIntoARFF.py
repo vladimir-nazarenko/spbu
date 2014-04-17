a505fafb9443d489b7347d9cd3d93a95c38b2674
Vladimir Nazarenko <nazarenko2010@gmail.com>
2014-04-17 12:56:34 +0400

    Rafactoring. Adding class

--- ConvertMNISTIntoARFF.py
+++ ConvertMNISTIntoARFF.py
@@ -10,6 +10,7 @@ from wand.image import Image
 # from wand.display import display
 from wand.color import Color
 from wand.drawing import Drawing
+from enum import Enum
 
 # Set to -1 if you want to use constant from file
 NUMBER_OF_IMAGES_TO_LOAD = 10000
@@ -19,31 +20,65 @@ STEP_OF_CHANGING_NOISE_RATE = 0.05
 IMAGE_OUTPUT_FILENAME = "digits.jpg"
 
 
-def convert_bytes_to_int32(data):
-    """Gets bytes and converts them into integer"""
-    return struct.unpack('i', data[::-1])[0]
+class MNISTConverter:
+    file_types = Enum('file_type', 'train test')
 
+    def __init__(self, test_images_path, test_labels_path, train_images_path, train_labels_path):
+        self.images = {'train': open(train_images_path, 'rb'),
+                       'test': open(test_images_path, 'rb')}
+        self.labels = {'train': open(train_labels_path, 'rb'),
+                       'test': open(test_labels_path, 'rb')}
+        for f in self.images.keys():
+            self.images[f] = {'file': self.images[f],
+                              'magicNumber': self.convert_bytes_to_int32(self.images[f].read(4)),
+                              'numberOfImages': self.convert_bytes_to_int32(self.images[f].read(4)),
+                              'rowCount': self.convert_bytes_to_int32(self.images[f].read(4)),
+                              'columnCount': self.convert_bytes_to_int32(self.images[f].read(4))}
+        for f in self.images.keys():
+            self.labels[f] = {'file': self.labels[f],
+                              'magicNumber': self.convert_bytes_to_int32(self.labels[f].read(4)),
+                              'numberOfLabels': self.convert_bytes_to_int32(self.labels[f].read(4))}
+        self.writer = None
 
-def read_one_image(images_file, labels_file=None, number_of_rows=28, number_of_columns=28):
-    """Read one image from file. Assumes headers were skipped. Returns list of bytes and byte with number type.
-        If labels_file is passed, adds label byte at the end"""
-    img = []
-    for i in range(number_of_rows):
-        for j in range(number_of_columns):
-            img.append(images_file.read(1)[0])
-    if labels_file is not None:
-        label = str(labels_file.read(1)[0])
-        img.append(label)
-    return img
+    def __enter__(self):
+        return self
 
+    def init_arff_writer(self, file_type, name='MNIST'):
+        """Set writer to appropriate position"""
+        print('Writing %s' % name)
+        col_names = []
+        for i in range(self.images['train']['columnCount']):
+            for j in range(self.images['train']['rowCount']):
+                col_names.append('x=%iy=%i' % (j, i))
+        col_names.append('class')
+        arff_writer = arff.Writer(name + '.arff', relation=name, names=col_names)
+        # set trigger: if we have str in column, interpret it as a class with such values
+        arff_writer.pytypes[str] = set([str(i) for i in range(10)])
+        self.writer = arff_writer
 
-def print_bytes(number, number_of_rows=28, number_of_columns=28):
+    @staticmethod
+    def convert_bytes_to_int32(self, data):
+        """Gets bytes and converts them into integer"""
+        return struct.unpack('i', data[::-1])[0]
+
+    def read_one_image(self):
+        """Read one image from file. Assumes headers were skipped. Returns list of bytes and byte with number type.
+            If labels_file is passed, adds label byte at the end"""
+        img = []
+        for i in range(self.images['train']['columnCount']):
+            for j in range(self.images['train']['rowCount']):
+                img.append(self.images['train']['file'].read(1)[0])
+        return img
+
+    def print_bytes(self, bytes):
     """Show picture of number in grey scale. Gets list of bytes, defining pixels in grey scale"""
     def skip_extra_parts(hex_str):
         """Gets string, representing hex number and skips part 0x"""
         return hex_str[2:]
     img = Image()
-    img = img.blank(number_of_columns, number_of_rows, background=Color('#ffffff'))
+    nrows = self.images['train']['rowCount']
+    ncols = self.images['train']['columnCount']
+    img = img.blank(ncols, number_of_rows, background=Color('#ffffff'))
     draw = Drawing()
     assert number_of_columns * number_of_rows == len(number)
     for r in range(number_of_rows):
@@ -53,6 +88,16 @@ def print_bytes(number, number_of_rows=28, number_of_columns=28):
             draw.line((c, r), (c, r))
     draw(img)
     img.save(filename=IMAGE_OUTPUT_FILENAME)
+
+    def __del__(self):
+        for f in self.files.itervalues():
+            f.close()
+
+
+
+
+
+
 
 
 def noise_pixels(pixels, ratio, contains_label=False):
@@ -74,18 +119,7 @@ def write_next_row(arff_writer, images, labels, noise_rate):
     arff_writer.write(pixels)
 
 
-def init_arff_writer(noise_rate, name='MNIST', numberofcolumns=28, numberofrows=28):
-    filename = '%s-NoiseRate%1.2f.arff' % (name, noise_rate)
-    print('Writing %s' % filename)
-    col_names = []
-    for i in range(numberofcolumns):
-        for j in range(numberofrows):
-            col_names.append('x=%iy=%i' % (j, i))
-    col_names.append('class')
-    arff_writer = arff.Writer(filename, relation='MNISTNoise%1.2f' % noise_rate, names=col_names)
-    # set trigger: if we have str in column, interpret it as a class with such values
-    arff_writer.pytypes[str] = set([str(i) for i in range(10)])
-    return arff_writer
+
 
 
 def check_number_of_rows(number_of_rows):
@@ -100,17 +134,34 @@ def check_number_of_rows(number_of_rows):
 def write_into_arff_with_constant_noise_rate(images, labels, noise_rate, number_of_images, name='MNIST', numberofcolumns=28, numberofrows=28):
     """Converts information from images and labels to arff"""
     assert 0 <= noise_rate <= 1.0
-    check_number_of_rows(number_of_images)
+    number_of_images = check_number_of_rows(number_of_images)
     writer = init_arff_writer(noise_rate, name, numberofcolumns, numberofrows)
     try:
         for i in range(number_of_images):
-            write_next_row()
+            write_next_row(writer, images, labels, noise_rate)
         print('OK\n')
     except IndexError:
         print('Error while reading from file\n')
     writer.close()
 
 
+def write_into_arff_with_unnoised_trainset(images, labels, noise_rate, number_of_images, name='MNIST', numberofcolumns=28, numberofrows=28):
+    """Converts information from images and labels to arff"""
+    assert 0 <= noise_rate <= 1.0
+    number_of_images = 3 * int(check_number_of_rows(number_of_images) / 3)
+    writer = init_arff_writer(noise_rate, name, numberofcolumns, numberofrows)
+    try:
+        for i in range(2 * int(number_of_images / 3)):
+            write_next_row(writer, images, labels, 0)
+        for i in range(int(number_of_images / 3)):
+            write_next_row(writer, images, labels, noise_rate)
+        print('OK\n')
+    except IndexError:
+        print('Error while reading from file\n')
+    writer.close()
+
+
+# TODO: return class with read values
 def skip_head(images):
     images.read(16)
 
@@ -131,21 +182,21 @@ def visualize_first_n_images(images_path, n, noise_rate):
         print_bytes(number=resultimage, number_of_rows=n * 28, number_of_columns=n * 28)
 
 
-def process_dataset(images_path, labels_path, noise_rate=0.0):
+def process_dataset(images_path, labels_path, noise_rate=0.0, mode="TrainWithNoNoise"):
+    assert mode == "TrainWithNoNoise" or mode == "TrainWithNoise"
     assert 0 <= noise_rate <= 1.0
     for i in range(NUMBER_OF_FILES_TO_GENERATE):
         with open(images_path, 'rb') as images, open(labels_path, 'rb') as labels:
             # Read values from images file
-            magicnumberimages = convert_bytes_to_int32(images.read(4))
-            numberofimages = convert_bytes_to_int32(images.read(4))
-            numberofrows = convert_bytes_to_int32(images.read(4))
-            numberofcolumns = convert_bytes_to_int32(images.read(4))
 
             # read values from labels file
             magicnumberlabels = convert_bytes_to_int32(labels.read(4))
             numberoflabels = convert_bytes_to_int32(labels.read(4))
             assert numberofimages == numberoflabels
-            write_into_arff_with_constant_noise_rate(images, labels, noise_rate, numberofimages)
+            if mode == "TrainWithNoNoise":
+                write_into_arff_with_constant_noise_rate(images, labels, noise_rate, numberofimages)
+            else:
+                write_into_arff_with_unnoised_trainset(images, labels, noise_rate, numberofimages)
             noise_rate += STEP_OF_CHANGING_NOISE_RATE
 
 
@@ -155,8 +206,9 @@ def main():
     train_labels_path = 'train-labels-idx1-ubyte'
     test_images_path = 't10k-images-idx3-ubyte'
     test_labels_path = 't10k-labels-idx1-ubyte'
-    # process_dataset(images_path=train_images_path, labels_path=train_labels_path, noise_rate=START_FROM_NOISE_RATE)
-    visualize_first_n_images(test_images_path, 5, 0.1)
+    process_dataset(images_path=train_images_path, labels_path=train_labels_path, noise_rate=START_FROM_NOISE_RATE,
+                    mode='TrainWithNoNoise')
+    # visualize_first_n_images(test_images_path, 5, 0.1)
     print("Exceeded %1.2f seconds" % (time.time() - start_time))
 
 
