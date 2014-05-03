391530e5b3c30dbbc46dd05bc6a8dbf9773901a8
Vladimir Nazarenko <nazarenko2010@gmail.com>
2014-05-03 19:27:45 +0400

    Added conclusions

---
+++ plot_c_change.R
@@ -0,0 +1,12 @@
+noiseRates <- 0:8
+noiseRates<- noiseRates / 10
+#plot perfomance degradation
+perf <- read.csv("perfomance_table.csv")
+perf <- perf[2:6]
+tim <- read.csv("time_training.csv")
+tim <- tim[2:6]
+pdf("graphics/c_param.pdf",width = 11 ,height = 5,family= "URWHelvetica", encoding="CP1251") 
+par(mfrow=c(1, 2))
+boxplot(perf, names=c(1.0, 0.1, 0.01, 0.001, 0.0001), xlab="Значение параметра", ylab="Процент верных")
+boxplot(log(tim), names=c(1.0, 0.1, 0.01, 0.001, 0.0001), xlab="Значение параметра", ylab="ln(время тренировки в секундах)")
+dev.off()
