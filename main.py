8341ec9f5c33427069c6917a6aba50b6235a8cb1
Vladimir Nazarenko <nazarenko2010@gmail.com>
2014-12-04 13:42:40 +0300

    Some progress at thresholding

--- main.py
+++ main.py
@@ -9,7 +9,12 @@ import cv2
 class Main:
     def __init__(self):
         self.cap = cv2.VideoCapture(0)
-
+        cv2.namedWindow('Thresholded')
+        cv2.createTrackbar('threshold', 'Thresholded', self._threshold, 255, self.set_threshold)
+        # cv2.createTrackbar('max value', 'Thresholded', self._max_value, 255, self.set_maxvalue)
+        # self.bgs = cv2.BackgroundSubtractorMOG2(10, 16, False)
+        self.output = open('out.txt', 'w')
+        self.write_statistics = False
 
     _threshold = 30
     _max_value = 255
@@ -20,8 +25,9 @@ class Main:
 
     def thresh(self, image):
         gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-        blur = cv2.GaussianBlur(gray, (5, 5), 10)
-        ret, thresh = cv2.threshold(blur, self._threshold, self._max_value, cv2.THRESH_BINARY)
+        blur = cv2.GaussianBlur(gray, (5, 5), 0)
+        ret, thresh = cv2.threshold(blur, self._threshold, self._max_value, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+        # thresh = cv2.adaptiveThreshold(blur, self._max_value, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY_INV, 5, 1)
         return thresh
 
     @staticmethod
@@ -39,7 +45,8 @@ class Main:
                 cnt = i
         return cnt
 
-
+    def draw_hull(self, img):
+        pass
 
     def set_threshold(self, x):
         self._threshold = x
@@ -53,8 +60,10 @@ class Main:
             cv2.imshow('Camera input', img)
             cv2.imshow('Thresholded', self.thresh(img))
             drawing = np.zeros(img.shape, np.uint8)
-            cnt = self.largest_contour(self.contours(self.thresh(img)))
+            for_proc = self.thresh(img)
+            cnt = self.largest_contour(self.contours(for_proc))
             if cnt is not None:
+
                 hull = cv2.convexHull(cnt)
                 cv2.drawContours(drawing, [cnt], 0, (0, 255, 0), 2)
                 cv2.drawContours(drawing, [hull], 0, (0, 0, 255), 2)
@@ -72,7 +81,13 @@ class Main:
                         far = tuple(cnt[f][0])
                         #dist = cv2.pointPolygonTest(cnt, centr, True)
                         cv2.line(drawing, start, end, [0, 255, 0], 2)
-                        cv2.circle(drawing, far, 5, [0, 0, 255], -1)
+                        cv2.circle(drawing, far, 5, [0, 0, 255], 2)
+                        if self.write_statistics:
+                            norm_x = (far[0] - center[0]) / radius
+                            norm_y = (far[1] - center[1]) / radius
+                            self.output.write('{0},{1};'.format(norm_x, norm_y))
+                    if self.write_statistics:
+                        self.output.write('\n')
 
                 except:
                     pass
@@ -83,6 +98,9 @@ class Main:
             k = cv2.waitKey(10)
             if k == 27:
                 break
+            elif k == 32:
+                self.write_statistics = not self.write_statistics
+        self.output.close()
 
 performer = Main()
 performer.bring_the_action()
