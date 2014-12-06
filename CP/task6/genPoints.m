27a48c595d3aa27ba192b66064d9293cf80173ff
Vladimir Nazarenko <nazarenko2010@gmail.com>
2014-12-06 19:30:59 +0300

    Homework 6 output fixed

--- CP/task6/genPoints.m
+++
@@ -1,8 +0,0 @@
-function points = genPoints(a, b, number)
-  assert(number > 1);
-  number -= 1;
-  assert(a < b);
-  points = 0:number;
-  points = a + points * (b - a) / number;
-endfunction
-  
--- CP/task6/integration.m
+++ CP/task6/integration.m
@@ -5,7 +5,6 @@ a = 0; #input("Enter the left margin: ");
 b = 1; #input("Enter the right margin: ");
 N = 3; #input("Enter the number of notes for formula: ");
 assert(N > 0);
-assert(a < b);
 # Comoute coefficients for ortagonal polynomial
 A = zeros(N);
 for i = 1:N
@@ -14,17 +13,24 @@ for i = 1:N
     end
 end
 B = -getMoment(N:(N * 2 - 1), a, b)';
-A
-B
-coeff = A \ B
-X = roots([1; coeff]')
-rts = zeros(N)
+puts "Moments of function: \n";
+disp(getMoment(0:(N * 2 - 1), a, b)');
+coeff = A \ B;
+puts "Ortogonal polynomial: \n";
+disp(polyout([1;coeff]', 'x'));
+X = roots([1; coeff]');
+rts = zeros(N);
 for i = 1:N
   for j = 1:N
     rts(i, j) = X(j) ^ (i - 1);
   endfor
 endfor
-rts
-Ak = rts \ getMoment(0:N-1, a, b)'
-calc = Ak' * f(X)
+puts "Knots of formula: \n";
+disp(X);
+Ak = rts \ getMoment(0:N-1, a, b)';
+puts "Coefficients of formula: \n";
+disp(Ak);
+puts "Result: ";
+calc = Ak' * f(X);
+disp(calc)
 format short
